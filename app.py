aae3f05a95933f735d7dc2f53960e9454c5aea72
Brian Aston <astonb.dev@gmail.com>
2022-01-26 16:52:48 -0800

    Finished main functionality of mad libs app

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
+base_story = silly_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-story = silly_story
+
 
 @app.get("/")
 @app.get("/questions")
 def show_homepage():
-    """Starts homepage"""
+    """Starts madlibs homepage"""
     
-    return render_template("questions.html", prompts=story.prompts)
+    return render_template("questions.html", prompts=base_story.prompts)
+
+@app.get("/results")
+def show_story():
+    """displays story from questions.html form"""
+
+    new_story = base_story.generate(request.args)
+
+    return render_template("story.html", story=new_story)
 
 
